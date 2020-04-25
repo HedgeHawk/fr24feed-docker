8bbc39b39e7ea58db30d03f2ac5e1d3fc426e556
HedgeHawk <darth-sidious@gmx.net>
2020-04-25 12:34:39 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat << EOF >> /etc/fr24feed.ini
+receiver="beast-tcp"
+fr24key="${FR24_KEY}"
+host="${DUMP1090_HOST}:30005"
+bs="no"
+raw="no"
+logmode="0"
+logpath="/var/log/fr24feed"
+mlat="yes"
+mlat-without-gps="yes"
+EOF
+
+if [ -n "${FR24_KEY}" ] && [ -n "${DUMP1090_HOST}" ]; then
+  fr24feed --key=${FR24_KEY} --host=${DUMP1090_HOST}:30005 --receiver=beast-tcp
+else
+  exit 1;
+fi
